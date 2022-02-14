afa263b4a1c1f4c9584f60165a9fd6e4c88de70c
wanglin <1013335014@qq.com>
2022-02-14 22:12:00 +0800

    完成

--- vue-cli-plugin-i18n/generator.js
+++ vue-cli-plugin-i18n/generator.js
@@ -2,14 +2,19 @@ const {
     exec
 } = require('child_process');
 
-module.exports = (api, options, rootOptions) => {
+module.exports = (api) => {
+    // 为了方便在项目里看到编译多语言的命令，我们把hello i18n添加到项目的package.json文件里，修改package.json文件可以使用提供的api.extendPackage方法
     api.extendPackage({
         scripts: {
             buildI18n: 'hello i18n'
         }
     })
+    // 该钩子会在文件写入硬盘后调用
     api.afterInvoke(() => {
-        exec(`cd ${rootOptions.projectName} && hello i18n`, (error, stdout, stderr) => {
+        // 获取项目的完整路径
+        let targetDir = api.generator.context
+        // 进入项目文件夹，然后运行命令
+        exec(`cd ${targetDir} && npm run buildI18n`, (error, stdout, stderr) => {
             if (error) {
                 console.error(error);
                 return;
